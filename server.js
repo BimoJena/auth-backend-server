c09ac7f7f21a2b5fb42bc600b905609f89b198d9
BimoJena <taskbimo@gmail.com>
2025-12-17 18:39:29 +0530

    add resolved cors error

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ connectDB()
 app.use(express.json())
 app.use(cookieParser())
 
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = ['https://auth-bimo.vercel.app']
+// const allowedOrigins = ['http://localhost:5173']
 
 app.use(cors({origin: allowedOrigins, credentials: true}))
 
